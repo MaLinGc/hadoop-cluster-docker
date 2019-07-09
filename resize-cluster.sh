262f46bca416dfbc3bef0030683056b995431445
maling <maling@scbit.org>
2019-07-09 18:14:00 +0800

    spark

--- resize-cluster.sh
+++ resize-cluster.sh
@@ -3,7 +3,7 @@
 # N is the node number of hadoop cluster
 N=$1
 
-if [ $# = 0 ]
+if [[ $# = 0 ]]
 then
 	echo "Please specify the node number of hadoop cluster!"
 	exit 1
@@ -12,11 +12,11 @@ fi
 # change slaves file
 i=1
 rm config/slaves
-while [ $i -lt $N ]
+while [[ ${i} -lt ${N} ]]
 do
 	echo "hadoop-slave$i" >> config/slaves
 	((i++))
-done 
+done
 
 echo ""
 
--- start-container.sh
+++ start-container.sh
@@ -11,6 +11,7 @@ sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
+                -p 9000:9000 \
                 --name hadoop-master \
                 --hostname hadoop-master \
                 kiwenlau/hadoop:1.0 &> /dev/null
@@ -18,17 +19,17 @@ sudo docker run -itd \
 
 # start hadoop slave container
 i=1
-while [ $i -lt $N ]
+while [[ ${i} -lt ${N} ]]
 do
-	sudo docker rm -f hadoop-slave$i &> /dev/null
+	sudo docker rm -f hadoop-slave${i} &> /dev/null
 	echo "start hadoop-slave$i container..."
 	sudo docker run -itd \
 	                --net=hadoop \
-	                --name hadoop-slave$i \
-	                --hostname hadoop-slave$i \
+	                --name hadoop-slave${i} \
+	                --hostname hadoop-slave${i} \
 	                kiwenlau/hadoop:1.0 &> /dev/null
 	i=$(( $i + 1 ))
-done 
+done
 
 # get into hadoop master container
 sudo docker exec -it hadoop-master bash
